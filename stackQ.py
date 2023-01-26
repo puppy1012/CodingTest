fddbf4d4260c01d870a7f8e61a631122b1b87fe9
Seunghyun Lee <puppy1012@naver.com>
2023-01-26 22:55:15 +0900

    스택,재귀함수 연습

--- stackQ.py
+++ stackQ.py
@@ -35,3 +35,19 @@ def recursive_function(i):
     print(i,'번째 재귀 함수를 종료합니다')
 
 recursive_function(1)
+
+#반복적으로 구현한 n!
+def factorial_iterative(n):
+    result =1
+    for i in range(1,n+1):
+        result *=i
+    return result
+
+#재귀적으로 구현한 n!
+def factorial_recursive(n):
+    if n<=1:
+        return 1
+    #n! = n*(n-1)!
+    return n* factorial_recursive(n-1)
+print('반복적 구현:',factorial_iterative(5))
+print('재귀적으로구현:',factorial_recursive(5))
