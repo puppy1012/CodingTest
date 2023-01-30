9fe78276415a92b968e44dbb385239e4b5d6b06e
Seunghyun Lee <puppy1012@naver.com>
2023-01-30 23:32:25 +0900

    스와프,정렬

--- sorting.py
+++ sorting.py
@@ -8,3 +8,18 @@ for i in range(len(array)):
     array[i],array[min_index]=array[min_index],array[i]
 
 print(array)
+
+array=[3,5]
+array[0],array[1] = array[1],array[0]
+print(array)
+
+array=[7,5,9,0,3,1,6,2,4,8]
+
+for i in range(1,len(array)):
+    #start,end,step i부터 0까지 -1만큼 진행하라
+    for j in range(i,0,-1):
+        if array[j]<array[j-1]:
+            array[j],array[j-1]=array[j-1],array[j]
+        else:
+            break
+print(array)
